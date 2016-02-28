e8f0a5c930b8f8caad8f2c90749962f0f58a1434
trummerjo <trummerjo@users.noreply.github.com>
2016-02-28 18:17:16 +0100

    Update client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -110,9 +110,9 @@ ADDON_STATUS ADDON_Create(void *hdl, void *props) {
 
     zatUsername = "";
     zatPassword = "";
-    ADDON_ReadSettings();
+    //ADDON_ReadSettings();
 XBMC->Log(LOG_DEBUG, "Create zat"");
-    zat = new ZatData(zatUsername,zatPassword);
+    //zat = new ZatData(zatUsername,zatPassword);
     XBMC->Log(LOG_DEBUG, "zat created"");
     m_CurStatus = ADDON_STATUS_OK;
     m_bCreated = true;
