4a501a8ed9e41c60034b2b087ca8f7d143eb8859
trummerjo <trummerjo@users.noreply.github.com>
2016-02-28 18:11:47 +0100

    Update ZatData.cpp

--- src/ZatData.cpp
+++ src/ZatData.cpp
@@ -401,15 +401,15 @@ ZatData::ZatData(std::string u, std::string p)  {
     //httpResponse response = getRequest("zattoo.com/deinemama");
     //cout << response.body;
 
-    this->loadAppId();
+   /* this->loadAppId();
     this->sendHello();
     if(this->login()) {
         this->loadChannels();
     }
     else {
         XBMC->QueueNotification(QUEUE_ERROR, "Zattoo Login fehlgeschlagen!");
-    }
-
+    }*/
+XBMC->QueueNotification(QUEUE_ERROR, "Created!");
 }
 
 ZatData::~ZatData() {
