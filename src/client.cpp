3db5fcba2da3564fba7a93df51a926b4afc8700c
vdrtuxnet <vdrtuxnet@users.noreply.github.com>
2016-11-23 00:08:00 +0100

    Update client.cpp
    Fix build for Android

--- src/client.cpp
+++ src/client.cpp
@@ -3,12 +3,15 @@
 #include "kodi/xbmc_pvr_dll.h"
 #include "kodi/libKODI_guilib.h"
 #include <iostream>
-#include "to_string.h"
 
 using namespace ADDON;
 
 #ifdef TARGET_WINDOWS
 #define snprintf _snprintf
+#endif
+
+#ifdef TARGET_ANDROID
+#include "to_string.h"
 #endif
 
 ADDON_STATUS m_CurStatus = ADDON_STATUS_UNKNOWN;
