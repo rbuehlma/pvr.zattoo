e90bfa5b12c27bb96675940382f542985c1d63cd
René Bühlmann <rene@buehlmann.net>
2019-05-21 21:37:56 +0200

    Improve loading xmltv file

--- src/XmlTV.cpp
+++ src/XmlTV.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+#include <algorithm>
 #include "XmlTV.h"
 #include <tinyxml2.h>
 #include <time.h>
@@ -7,8 +9,10 @@
 using namespace tinyxml2;
 using namespace ADDON;
 
+const time_t minimumUpdateDelay = 600;
+
 XmlTV::XmlTV(std::string xmlFile) :
-  m_xmlFile(xmlFile)
+  m_xmlFile(xmlFile), m_lastUpdate(0)
 {
   if (!XBMC->FileExists(m_xmlFile.c_str(), true))
   {
@@ -21,13 +25,21 @@ XmlTV::XmlTV(std::string xmlFile) :
     XBMC->Log(LOG_DEBUG, "XMLTV: Using xml file for additional guide data: %s",
         m_xmlFile.c_str());
   }
+}
 
+XmlTV::~XmlTV()
+{
+  m_xmlFile = "";
 }
 
 bool XmlTV::GetEPGForChannel(const std::string &cid, unsigned int uniqueChannelId)
 {
-  if (!XBMC->FileExists(m_xmlFile.c_str(), true))
+  if (!m_xmlFile.empty() && !XBMC->FileExists(m_xmlFile.c_str(), true))
   {
+    return false;
+  }
+  
+  if (!isUpdateDue()) {
     return false;
   }
 
@@ -86,6 +98,45 @@ bool XmlTV::GetEPGForChannel(const std::string &cid, unsigned int uniqueChannelI
       tag.iGenreType = EPG_GENRE_USE_STRING;
       tag.strGenreDescription = genre;
     }
+    
+    // episode is deliverd in format "1 . 2 .". Try to parse that format
+    XMLElement* episodeItem = programme->FirstChildElement("episode-num");
+    if (episodeItem != nullptr) {
+      std::string episode = episodeItem->GetText();
+      episode.erase (std::remove(episode.begin(), episode.end(), ' '), episode.end());
+      std::replace(episode.begin(), episode.end(), '.', ' ');
+      std::stringstream ss(episode);
+      int number;
+      if (ss >> number) {
+        tag.iSeriesNumber = number + 1;
+        if (ss >> number) {
+          tag.iEpisodeNumber = number + 1;
+        }
+      }
+    }
+    
+    XMLElement* ratingItem = programme->FirstChildElement("rating");
+    if (ratingItem != nullptr) {
+      XMLElement* valuteItem = ratingItem->FirstChildElement("value");
+      if (valuteItem != nullptr) {
+        std::string ratingString = valuteItem->GetText();
+        tag.iParentalRating  = std::stoi(ratingString);
+      }
+    }
+    
+    XMLElement* iconItem = programme->FirstChildElement("icon");
+    if (iconItem != nullptr) {
+      tag.strIconPath = iconItem->Attribute("src", 0);
+    }
+    
+    XMLElement* starRatingItem = programme->FirstChildElement("star-rating");
+    if (starRatingItem != nullptr) {
+      XMLElement* valuteItem = starRatingItem->FirstChildElement("value");
+      if (valuteItem != nullptr) {
+        std::string ratingString = valuteItem->GetText();
+        tag.iStarRating  = std::stoi(ratingString);
+      }
+    }
 
     result = true;
     PVR->EpgEventStateChange(&tag, EPG_EVENT_CREATED);
@@ -95,6 +146,16 @@ bool XmlTV::GetEPGForChannel(const std::string &cid, unsigned int uniqueChannelI
   }
   return result;
 
+}
+
+bool XmlTV::isUpdateDue() {
+  time_t currentTime;
+  time(&currentTime);
+  if (m_lastUpdate + minimumUpdateDelay > currentTime) {
+    return false;
+  }
+  m_lastUpdate = currentTime;
+  return true;
 }
 
 time_t XmlTV::StringToTime(const std::string &timeString)
